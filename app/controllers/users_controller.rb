75e760bc05e639da40907f2b2088db8693c3f0df
JasonEb <jasonebueng@gmail.com>
2020-12-29 22:41:50 -0800

    adds User migration, model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
     def index
         render plain: "Hello world!"
     end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :username))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: params
+    end
 end
