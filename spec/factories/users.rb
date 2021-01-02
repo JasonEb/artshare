c72addedbfdc9b57880d9446979bdf23eaa3e980
JasonEb <jasonebueng@gmail.com>
2021-01-01 22:40:57 -0800

    fixes nested user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name 'Jerry'
-    username 'Test1234'
+
+    factory :test_user do
+      name {'Jerry'}
+      username {'Test1234'}
+    end
   end
 
-  factory :blank_user do
-  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  let(:blank_user) { FactoryBot.build(:blank_user) }
+  let(:user) { FactoryBot.build(:user) }
 
   it 'validates presence of name' do
-    expect(blank_user).not_to be_valid
+    expect(user).not_to be_valid
 
-    blank_user.name = "Jerry"
-    blank_user.username = "Test1234"
+    user.name = "Jerry"
+    user.username = "Test1234"
     expect(user).to be_valid
   end
 end
