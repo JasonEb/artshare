791324d8f1f4621d3e8cb3fb95ee89676f29cf55
JasonEb <jasonebueng@gmail.com>
2021-01-02 20:44:44 -0800

    adds artwork model specs

--- spec/models/artwork_spec.rb
+++ spec/models/artwork_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Artwork, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:artwork) { FactoryBot.build(:artwork )}
+
+  it 'validates the presence of title, image_url, and curator_id' do
+    expect(:artwork).not_to be_valid
+
+    artwork.title = "test title"
+    expect(:artwork).not_to be_valid
+    artwork.image_url = "www.localhost.com/example.jpeg"
+    expect(:artwork).not_to be_valid
+    artwork.curator_id = 0
+    expect(:artwork).to be_valid
+  end
 end
